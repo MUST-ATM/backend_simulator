e485af1b8d2057802f1d3abe0493ff33e2215f0c
小火 <49596040+xiaohuo-desu@users.noreply.github.com>
2024-11-19 00:11:37 +0800

    update

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ async def faceAntiSimulator(request: Request):
         async with aiofiles.open(filename, 'wb') as f:
             async for chunk in request.stream():
                 await f.write(chunk)
-        if(filename == "True.png"):
+        if(filename == "capture.jpg"):
             return HTTPException(status_code=200, detail="Success")
         else:
             raise HTTPException(status_code=411, detail="Error FaceAntiSpoofing")
@@ -43,6 +43,16 @@ async def deposit(request: Request):
         raise HTTPException(status_code=405, detail="Balance not found")
     raise HTTPException(status_code=200, detail="Success")
 
+@app.post("/account/balance/change", tags=["balance"])
+async def change(request: Request):
+    data = await request.json()
+    try:
+        user_id = data.get("user_id")
+        currency = data.get("currency")
+        amount = data.get("amount")
+    except Exception:
+        raise HTTPException(status_code=405, detail="Balance not found")
+    raise HTTPException(status_code=200, detail="Success")
 # Get function
 @app.get("/account/face/{faceId}")
 async def userIdSimulator(faceId: str):
@@ -50,4 +60,14 @@ async def userIdSimulator(faceId: str):
 
 @app.get("/account/card/{userId}/{currency}")
 async def balanceSimulator(userId: str, currency: str):
-    return {"balance": 1000}
+    return {"balance": 2333}
+
+#ping-pong
+@app.get("/ping")
+async def pong():
+    return {"ping": "pong"}
+
+@app.post("/ping")
+async def pong(request: Request):
+    data = await request.json()
+    return {"pong": data}
